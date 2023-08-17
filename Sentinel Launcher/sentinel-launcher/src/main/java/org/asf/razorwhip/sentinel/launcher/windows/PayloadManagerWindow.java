d7d574ac2fb3659990d7876234e72e46857fb0bc
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2023-08-17 12:23:25 +0200

    Implement payload imports in the payload manager

--- Sentinel Launcher/sentinel-launcher/src/main/java/org/asf/razorwhip/sentinel/launcher/windows/PayloadManagerWindow.java
+++ Sentinel Launcher/sentinel-launcher/src/main/java/org/asf/razorwhip/sentinel/launcher/windows/PayloadManagerWindow.java
@@ -11,6 +11,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JDialog;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
@@ -46,6 +47,7 @@ import javax.swing.ListSelectionModel;
 import javax.swing.JList;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.filechooser.FileFilter;
 
 import org.asf.razorwhip.sentinel.launcher.LauncherUtils;
 import org.asf.razorwhip.sentinel.launcher.api.PayloadType;
@@ -478,8 +480,7 @@ public class PayloadManagerWindow extends JDialog {
 
 													// Add the payload
 													// Should throw exception on any errors
-													// TODO
-													payloads = payloads;
+													importPayload(tempOut, true);
 
 													// Success
 													// Set enabled
@@ -606,7 +607,33 @@ public class PayloadManagerWindow extends JDialog {
 		});
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				// TODO
+				// Show window
+				JFileChooser chooser = new JFileChooser();
+				FileFilter filter = new FileFilter() {
+
+					@Override
+					public boolean accept(File f) {
+						return f.isDirectory() || f.getName().endsWith(".spf");
+					}
+
+					@Override
+					public String getDescription() {
+						return "Sentinel payload files (*.spf)";
+					}
+
+				};
+				chooser.addChoosableFileFilter(filter);
+				chooser.setFileFilter(filter);
+				if (chooser.showDialog(PayloadManagerWindow.this, "Select") == JFileChooser.APPROVE_OPTION) {
+					// Check file
+					File spf = chooser.getSelectedFile();
+					if (spf != null) {
+						try {
+							importPayload(spf, false);
+						} catch (IOException e1) {
+						}
+					}
+				}
 			}
 		});
 		btnRemove.addActionListener(new ActionListener() {
@@ -653,6 +680,162 @@ public class PayloadManagerWindow extends JDialog {
 				dispose();
 			}
 		});
+	}
+
+	protected boolean importPayload(File spf, boolean throwError) throws IOException {
+		try {
+			// Read descriptor
+			Map<String, String> descriptor = LauncherUtils.parseProperties(getStringFrom(spf, "payloadinfo"));
+
+			// Verify descriptor validity
+			String type = "Full";
+			if (descriptor.containsKey("Type"))
+				type = descriptor.get("Type");
+			type = type.toLowerCase();
+			if (!type.equals("resource") && !type.equals("full")) {
+				// Incompatible
+				JOptionPane.showMessageDialog(PayloadManagerWindow.this,
+						"The payload you selected has an invalid type!", "Error", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+			if (descriptor.containsKey("Game-ID")) {
+				// Check
+				if (!descriptor.get("Game-ID").equalsIgnoreCase(LauncherUtils.getGameID())) {
+					// Incompatible
+					if (throwError)
+						throw new IOException("Incompatible payload");
+					JOptionPane.showMessageDialog(PayloadManagerWindow.this,
+							"The payload you selected is not compatible with the game or emulation software!", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return false;
+				}
+			}
+			if (descriptor.containsKey("Software-ID")) {
+				// Check
+				if (!descriptor.get("Software-ID").equalsIgnoreCase(LauncherUtils.getSoftwareID())) {
+					// Incompatible
+					if (throwError)
+						throw new IOException("Incompatible payload");
+					JOptionPane.showMessageDialog(PayloadManagerWindow.this,
+							"The payload you selected is not compatible with the game or emulation software!", "Error",
+							JOptionPane.ERROR_MESSAGE);
+					return false;
+				}
+			}
+
+			// Load ID
+			String id = spf.getName();
+			if (descriptor.containsKey("Payload-ID"))
+				id = descriptor.get("Payload-ID");
+
+			// Check
+			if (payloads.containsKey(id)) {
+				// Incompatible
+				if (throwError)
+					throw new IOException("Payload conflict");
+				JOptionPane.showMessageDialog(PayloadManagerWindow.this, "The payload you selected is already present!",
+						"Error", JOptionPane.ERROR_MESSAGE);
+				return false;
+			}
+
+			// Add
+			String name = id + ".spf";
+			int i = 1;
+			while (new File("payloads", name).exists()) {
+				name = id + "-" + i++ + ".spf";
+			}
+
+			// Copy file
+			Files.copy(spf.toPath(), new File("payloads", name).toPath());
+			spf = new File("payloads", name);
+
+			// Add
+			PayloadEntry payload = new PayloadEntry();
+			payload.id = name;
+			payload.name = name;
+			payload.version = "default";
+			payload.file = name;
+			payload.enabled = false;
+
+			// Set type
+			payload.type = type.equals("resource") ? PayloadType.RESOURCE : PayloadType.PAYLOAD;
+
+			// Load settings
+			if (descriptor.containsKey("Payload-ID")) {
+				payload.id = descriptor.get("Payload-ID");
+				payload.name = payload.id;
+			}
+			if (descriptor.containsKey("Payload-Name"))
+				payload.name = descriptor.get("Payload-Name");
+			if (descriptor.containsKey("Payload-Version"))
+				payload.version = descriptor.get("Payload-Version");
+
+			// Load dependencies
+			payload.dependencies = new PayloadDependency[0];
+			payload.conflictsWith = new PayloadDependency[0];
+			try {
+				JsonObject depsConfig = JsonParser.parseString(getStringFrom(spf, "dependencies.json"))
+						.getAsJsonObject();
+
+				// Prepare lists
+				ArrayList<PayloadDependency> deps = new ArrayList<PayloadDependency>();
+				ArrayList<PayloadDependency> conflicts = new ArrayList<PayloadDependency>();
+
+				// Read from config
+				if (depsConfig.has("dependencies")) {
+					for (JsonElement ele : depsConfig.get("dependencies").getAsJsonArray()) {
+						JsonObject dep = ele.getAsJsonObject();
+
+						// Add
+						PayloadDependency dependency = new PayloadDependency();
+						dependency.id = dep.get("id").getAsString();
+						if (dep.has("version")) {
+							dependency.version = dep.get("version").getAsString();
+							if (dep.has("versionString"))
+								dependency.versionString = dep.get("versionString").getAsString();
+							else
+								dependency.versionString = dependency.version;
+						}
+						if (dep.has("url"))
+							dependency.url = dep.get("url").getAsString();
+						deps.add(dependency);
+					}
+				}
+				if (depsConfig.has("conflicts")) {
+					for (JsonElement ele : depsConfig.get("conflicts").getAsJsonArray()) {
+						JsonObject dep = ele.getAsJsonObject();
+
+						// Add
+						PayloadDependency conflict = new PayloadDependency();
+						conflict.id = dep.get("id").getAsString();
+						if (dep.has("version"))
+							conflict.version = dep.get("version").getAsString();
+						conflicts.add(conflict);
+					}
+				}
+
+				// Apply
+				payload.dependencies = deps.toArray(t -> new PayloadDependency[t]);
+				payload.conflictsWith = conflicts.toArray(t -> new PayloadDependency[t]);
+			} catch (IOException e) {
+			}
+
+			// Create box
+			payload.box = new JCheckBox(payload.name + " (" + payload.type.toString().toLowerCase() + ")");
+
+			// Add
+			payloads.put(payload.id, payload);
+
+			// Refresh
+			refreshPayloadList();
+		} catch (Exception e2) {
+			if (throwError)
+				throw new IOException("Incompatible payload");
+			JOptionPane.showMessageDialog(PayloadManagerWindow.this, "Failed to load payload file!", "Error",
+					JOptionPane.ERROR_MESSAGE);
+			return false;
+		}
+		return true;
 	}
 
 	private void refreshPayloadList() {
