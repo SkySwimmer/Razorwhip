b46b06b19908ddb143e33af5cc99e09d70911f7a
Sky Swimmer <stefaneekhof@aerialworks.ddns.net>
2023-10-12 15:17:44 +0200

    Update versions so that connective gets rebuilt

--- Sentinel Launcher/sentinel-launcher/src/main/java/org/asf/razorwhip/sentinel/launcher/LauncherMain.java
+++ Sentinel Launcher/sentinel-launcher/src/main/java/org/asf/razorwhip/sentinel/launcher/LauncherMain.java
@@ -54,7 +54,7 @@ import java.awt.FlowLayout;
 
 public class LauncherMain {
 
-	public static final String LAUNCHER_VERSION = "1.0.0.A17";
+	public static final String LAUNCHER_VERSION = "1.0.0.A18";
 
 	JFrame frmSentinelLauncher;
 	private JLabel lblStatusLabel;
